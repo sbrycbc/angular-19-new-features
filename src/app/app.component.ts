7978e60080dead3de73c6a3f9ac17ad5883151cf
sbrycbc <sabriyecbc@gmail.com>
2024-12-30 11:21:34 +0100

    vordem updated...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [],
+  template:`
+  <h1>Angular v19</h1>
+  <input type="text">
+  <p>deneme version</p>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
